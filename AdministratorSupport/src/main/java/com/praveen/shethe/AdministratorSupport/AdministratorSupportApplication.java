5f2c8eb0ffceb4d0da48c75a6c018b391d20e318
Praveenkumar <praveen.shethe7@gmail.com>
2020-08-19 11:45:55 +0530

    #MS-02 Added updated gradle build dependencies

--- AdministratorSupport/src/main/java/com/praveen/shethe/AdministratorSupport/AdministratorSupportApplication.java
+++ AdministratorSupport/src/main/java/com/praveen/shethe/AdministratorSupport/AdministratorSupportApplication.java
@@ -2,12 +2,14 @@ package com.praveen.shethe.AdministratorSupport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class AdministratorSupportApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdministratorSupportApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdministratorSupportApplication.class, args);
+    }
 
 }
--- CustomerService/src/main/java/com/praveen/shethe/CustomerService/controller/CustomerController.java
+++ CustomerService/src/main/java/com/praveen/shethe/CustomerService/controller/CustomerController.java
@@ -3,5 +3,6 @@ package com.praveen.shethe.CustomerService.controller;
 /**
  * Created by Praveenkumar on 8/19/2020.
  */
+@RestController
 public class CustomerController {
 }
