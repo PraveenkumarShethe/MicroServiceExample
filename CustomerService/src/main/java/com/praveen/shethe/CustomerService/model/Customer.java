ceadc241424797023a4e4cc9e50450fce01cde06
Praveenkumar <praveen.shethe7@gmail.com>
2020-08-21 00:30:41 +0530

    #AS-01 customer fields added

--- CustomerService/src/main/java/com/praveen/shethe/CustomerService/model/Customer.java
+++ CustomerService/src/main/java/com/praveen/shethe/CustomerService/model/Customer.java
@@ -4,4 +4,64 @@ package com.praveen.shethe.CustomerService.model;
  * Created by Praveenkumar on 8/20/2020.
  */
 public class Customer extends AbstractEntity{
+
+    private String name;
+
+    private String middleName;
+
+    private String lastName;
+
+    private String mobileNo;
+
+    private String address;
+
+    private String landmark;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLandmark() {
+        return landmark;
+    }
+
+    public void setLandmark(String landmark) {
+        this.landmark = landmark;
+    }
 }
