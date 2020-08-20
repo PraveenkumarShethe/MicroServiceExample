d9413ce1aff902f1103fe81539a9aa433ce0fdeb
Praveenkumar <praveen.shethe7@gmail.com>
2020-08-20 15:04:09 +0530

    #MS-02 added with controller and model in general setup

---
+++ AdministratorSupport/src/main/java/com/praveen/shethe/AdministratorSupport/model/AbstractEntity.java
@@ -0,0 +1,49 @@
+package com.praveen.shethe.AdministratorSupport.model;
+
+/**
+ * Created by Praveenkumar on 8/20/2020.
+ * The abstract class for all entity classes. This provides the unique id to
+ * identify a row in the SQL db.
+ * <p>
+ */
+public class AbstractEntity {
+
+    private Long id;
+
+    /**
+     * Returns the identifier of the entity.
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (this.id == null || obj == null || !(this.getClass().equals(obj.getClass()))) {
+            return false;
+        }
+
+        AbstractEntity that = (AbstractEntity) obj;
+
+        return this.id.equals(that.getId());
+    }
+
+    /**
+     * (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id.hashCode();
+    }
+}
---
+++ AdministratorSupport/src/main/java/com/praveen/shethe/AdministratorSupport/model/customer/Customer.java
@@ -0,0 +1,9 @@
+package com.praveen.shethe.AdministratorSupport.model.customer;
+
+import com.praveen.shethe.AdministratorSupport.model.AbstractEntity;
+
+/**
+ * Created by Praveenkumar on 8/20/2020.
+ */
+public class Customer extends AbstractEntity {
+}
