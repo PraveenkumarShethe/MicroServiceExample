1b24906620033df31566a58775530cf40a306049
Praveenkumar <praveen.shethe7@gmail.com>
2020-09-07 00:55:50 +0530

    #SGW service RestTamplete implementation

--- SingleGateWayService/src/main/java/com/example/SingleGateWayService/service/RestTemplete.java
+++ SingleGateWayService/src/main/java/com/example/SingleGateWayService/service/RestTemplete.java
@@ -1,11 +1,23 @@
 package com.example.SingleGateWayService.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by Praveenkumar on 9/7/2020.
  */
+@Service
 public class RestTemplete {
 
-    
+    private static RestTemplete restTemplete = null;
+
+    private RestTemplete(){}
+
+    public RestTemplete getRestTemplete(){
+        if (restTemplete == null){
+            restTemplete = new RestTemplete();
+        }
+        return restTemplete;
+    }
 
 
 }
