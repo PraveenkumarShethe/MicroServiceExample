5e14df4eb6bbd876e4266c03d57913fa8a90cf64
Praveenkumar <praveen.shethe7@gmail.com>
2020-09-07 00:29:56 +0530

    #SGW service implementation

---
+++ SingleGateWayService/src/main/java/com/example/SingleGateWayService/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package com.example.SingleGateWayService.controller;
+
+import com.example.SingleGateWayService.model.customer.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by Praveenkumar on 9/7/2020.
+ */
+@RestController
+@RequestMapping(value = "/customer", produces = MediaType.APPLICATION_JSON_VALUE)
+public class CustomerController {
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    @Transactional(Transactional.TxType.NEVER)
+    public void createCustomer(@RequestBody Customer customer){
+
+    }
+
+}
