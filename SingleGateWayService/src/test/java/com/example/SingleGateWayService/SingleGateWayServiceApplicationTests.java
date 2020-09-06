f25e405de63660c6f065b0c74535f565552a5f7b
Praveenkumar <praveen.shethe7@gmail.com>
2020-09-07 00:04:23 +0530

    #SGW service added to repo

---
+++ SingleGateWayService/src/test/java/com/example/SingleGateWayService/SingleGateWayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SingleGateWayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleGateWayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
