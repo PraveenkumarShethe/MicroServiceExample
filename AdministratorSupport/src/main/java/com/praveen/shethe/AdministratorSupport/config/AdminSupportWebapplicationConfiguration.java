7aef0bf49cff09debc456fee8c0d9740d9426c5c
Praveenkumar <praveen.shethe7@gmail.com>
2020-10-02 21:40:30 +0530

    #AS-01 #time 3hours Added application.yml properties and also resolved some errors

--- AdministratorSupport/src/main/java/com/praveen/shethe/AdministratorSupport/config/AdminSupportWebapplicationConfiguration.java
+++ AdministratorSupport/src/main/java/com/praveen/shethe/AdministratorSupport/config/AdminSupportWebapplicationConfiguration.java
@@ -1,10 +1,14 @@
 package com.praveen.shethe.AdministratorSupport.config;
 
+import org.springframework.boot.env.YamlPropertySourceLoader;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.servlet.Filter;
+import java.io.IOException;
 
 /**
  * Created by Praveenkumar on 8/21/2020.
@@ -24,5 +28,12 @@ public class AdminSupportWebapplicationConfiguration {
         characterEncodingFilter.setEncoding("UTF-8");
         characterEncodingFilter.setForceEncoding(true);
         return characterEncodingFilter;
+    }
+
+    @Bean
+    public PropertySource yamlPropertySourceLoader() throws IOException {
+        YamlPropertySourceLoader yamlPropertySourceLoader = new YamlPropertySourceLoader();
+        return (PropertySource) yamlPropertySourceLoader.load("application.yml",
+                new ClassPathResource("application.yml"));
     }
 }
--- AdministratorSupport/src/main/java/com/praveen/shethe/AdministratorSupport/controller/AdminController.java
+++ AdministratorSupport/src/main/java/com/praveen/shethe/AdministratorSupport/controller/AdminController.java
@@ -1,7 +1,26 @@
 package com.praveen.shethe.AdministratorSupport.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+
 /**
  * Created by Praveenkumar on 8/19/2020.
  */
+@RestController
+@RequestMapping(value = "/admincontrol", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AdminController {
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    @Transactional(Transactional.TxType.NEVER)
+    public String getAllBooks() {
+        return "adminSupport is up";
+    }
+
 }
